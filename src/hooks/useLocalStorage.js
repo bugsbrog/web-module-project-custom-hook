5e8cac6fcdad4c45ed297f4aefdc4b03a092c9f3
bugsbrog <bugsbrog@gmail.com>
2021-12-01 13:51:17 -0700

    Done with everything I think

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,25 +1,21 @@
-import { useState } from 'react';
+import {useState} from "react";
 
 const useLocalStorage = (key, initialValue) => {
-    //1. when assigning initialState, check to see if a localStorage value exists.
-    //2. If a localStorage value exists, make that our initial value
-    //3. If a localStorage value does NOT exist, put initialValue into localStorage.
-    //4. Any time we set state, also set localStorage
-    const [value, setValue] = useState(()=> {
-        if (localStorage.getItem(key)) {
-            return JSON.parse(localStorage.getItem(key));
+    const [value, setValue] = useState(() => {
+        if(localStorage.getItem(key)){
+            return JSON.parse(localStorage.getItem(key))
         } else {
             localStorage.setItem(key, JSON.stringify(initialValue));
-            return(initialValue);
+            return(initialValue)
         }
     });
 
-    const setStoredValue = (value)=> {
+    const setStoredValue = value => {
         localStorage.setItem(key, JSON.stringify(value));
-        setValue(value);
-    }
+        setValue(value)
+    };
 
     return [value, setStoredValue];
-}
+};
 
 export default useLocalStorage;
